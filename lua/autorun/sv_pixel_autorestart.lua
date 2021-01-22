8dd097aa811d76c4fa07f67b6456795b83fa41d8
Tom <tom@tomdotbat.dev>
2021-01-22 00:05:24 +0000

    Discord and Pterodactyl API communication

---
+++ lua/autorun/sv_pixel_autorestart.lua
@@ -0,0 +1,67 @@
+
+local config = {
+    pterodactyl = {
+        apiKey = "***REMOVED***",
+        apiEndpoint = "***REMOVED***",
+        serverId = "27c2b5b6" --"1a7ce997"
+    },
+    discord = {
+        publicWebhook = "",
+        adminWebhook = "",
+        username = "PIXEL Auto-Restart",
+        avatarUrl = nil
+    }
+}
+
+local autoRestart = {}
+
+function autoRestart:sendRestartSignal()
+    local conf = config.pterodactyl
+    HTTP({
+        ["url"] = string.format("%s/client/servers/%s/power", conf.apiEndpoint, conf.serverId),
+        ["method"] = "POST",
+        ["headers"] = {
+            ["Accept"] = "application/json",
+            ["Content-Type"] = "application/json",
+            ["Authorization"] = "Bearer " .. conf.apiKey
+        },
+        ["body"] = "{\"signal\": \"restart\"}",
+        ["failed"] = function(reason)
+            self:sendDiscordAdminMessage("Failed to reboot the server via Pterodactyl API:\n" .. reason)
+        end
+    })
+end
+
+do
+    if pcall(function() require("chttp") end) then
+        local conf = config.discord
+        local function sendRequest(url, message)
+            CHTTP({
+                url = url,
+                body = string.format("{\"username\":%s,\"avatar_url\":%s,\"content\":%s}",
+                    conf.username, conf.avatarUrl, message),
+                method = "POST",
+                type = "application/json"
+            })
+        end
+
+        function autoRestart:sendDiscordMessage(message)
+            sendRequest(publicWebhook, message)
+        end
+
+        function autoRestart:sendDiscordAdminMessage(message)
+            sendRequest(adminWebhook, message)
+        end
+    else
+        print("Auto Restart: CHTTP not found, server chat/console as fallback.")
+        function autoRestart:sendDiscordMessage(message)
+            message = "Auto Restart: " .. message
+            PrintMessage(HUD_PRINTTALK, message)
+            print(message)
+        end
+
+        function autoRestart:sendDiscordAdminMessage(message)
+            print("Auto Restart: " .. message)
+        end
+    end
+end
