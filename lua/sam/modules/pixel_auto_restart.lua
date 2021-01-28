aebfad67c6afa3c909024f521d80edbd5ca2ae28
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-28 11:40:14 +0000

    Add a cancel restart command

---
+++ lua/sam/modules/pixel_auto_restart.lua
@@ -0,0 +1,13 @@
+
+sam.command.new("cancelrestart")
+    :SetCategory("PIXEL AutoRestart")
+    :Help("Cancels a forced restart.")
+    :SetPermission("cancelrestart", "superadmin")
+
+    :OnExecute(function(caller)
+        if not AutoRestart then return end
+        AutoRestart.CancelRestart(caller)
+
+        sam.player.send_message(nil, "{A} cancelled the restart.", {A = caller})
+    end)
+:End()
