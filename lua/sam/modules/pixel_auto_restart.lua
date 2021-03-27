a2d5485c7e2af7d16bb3839dd0ccc38a520f00b3
blu <blu@bluiscool.dev>
2021-03-26 23:37:55 -0500

    Add a queue restart command

--- lua/sam/modules/pixel_auto_restart.lua
+++ lua/sam/modules/pixel_auto_restart.lua
@@ -9,6 +9,8 @@ sam.command.new("cancelrestart")
     :OnExecute(function(caller)
         if not AutoRestart then return end
         AutoRestart.CancelRestart(caller)
+        timer.Remove("PIXEL.AutoRestart.RebootTimer")
+        timer.Remove("PIXEL.AutoRestart.ChatTimer")
 
         sam.player.send_message(nil, "{A} cancelled the restart.", {A = caller})
     end)
@@ -24,5 +26,37 @@ sam.command.new("restart")
         AutoRestart.StartRestart(caller)
 
         sam.player.send_message(nil, "{A} started a restart.", {A = caller})
+    end)
+:End()
+
+sam.command.new("queuerestart")
+    :SetCategory("PIXEL AutoRestart")
+    :Help("Start a restart after the provided minutes")
+    :SetPermission("restart", "superadmin")
+	:AddArg("length", {optional = false, default = 5})
+
+    :OnExecute(function(caller, length)
+        if not AutoRestart then return end
+        local timeUntilRestart = length * 60
+        
+        timer.Create("PIXEL.AutoRestart.RebootTimer", timeUntilRestart, 1, function()
+            AutoRestart.StartRestart(caller)
+        end)
+        
+        local timeLeft = timeUntilRestart
+        local timeSplit = timeUntilRestart / 3
+        
+        timer.Create("PIXEL.AutoRestart.ChatTimer", timeSplit, 3, function()
+            timeLeft = timeLeft - timeSplit
+            for _, ply in ipairs(player.GetAll()) do
+                ply:ChatPrint("The server will be restarting in " .. math.Round(length - timeLeft / 60, 1) .. " minutes")
+            end
+        end)
+
+        for _, ply in ipairs(player.GetAll()) do
+            ply:ChatPrint("The server will be restarting in " .. length .. " minutes")
+        end
+
+        sam.player.send_message(nil, "{A} started a restart for " .. length .. " minutes from now.", {A = caller})
     end)
 :End()
